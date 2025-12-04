e5aacd41d23d125db90a18aff2422b1036c760fc
Christopher DeStefano <cdeste1@gmail.com>
2025-12-03 19:49:42 -0500

    fix gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,20 +29,20 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     signingConfigs {
         create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String?
-            keyPassword = keystoreProperties["keyPassword"] as String?
-            storeFile = file(keystoreProperties["storeFile"] as String)
-            storePassword = keystoreProperties["storePassword"] as String?
+            keyAlias = keystoreProperties["keyAlias"] as? String ?: ""
+            keyPassword = keystoreProperties["keyPassword"] as? String ?: ""
+            storeFile = keystoreProperties["storeFile"]?.let { file(it as String) }
+            storePassword = keystoreProperties["storePassword"] as? String ?: ""
         }
     }
 
@@ -52,6 +52,9 @@ android {
             isMinifyEnabled = false
             isShrinkResources = false
         }
+         getByName("debug") {
+        signingConfig = signingConfigs.getByName("debug")
+    }
     }
 }
 
